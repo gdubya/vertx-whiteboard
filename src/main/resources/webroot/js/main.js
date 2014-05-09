71dba36a023bee9f710f6fad2a3bd8cbecbd2d1b
Gareth Western <gareth@garethwestern.com>
2014-05-09 23:16:42 +0200

    Ensure app is  loaded even before eb.onconnect

--- src/main/resources/webroot/js/main.js
+++ src/main/resources/webroot/js/main.js
@@ -2,40 +2,39 @@ function init(){
     var eb = new vertx.EventBus('http://localhost:8181/bus');
     var uuid = Math.random().toString(36).slice(2).substring(0, 6);
 
-    eb.onopen = function() {
-        window.app = new WhiteboardApp(eb, uuid);
+    window.app = new WhiteboardApp(eb, uuid);
 
+    eb.onopen = function() {
         eb.registerHandler('whiteboard', function(message) {
             var msg = JSON.parse(message);
             var fromOutside = msg.uuid !== uuid;
             if (fromOutside) {
                 if (msg.type === 'clear') {
-                    window.app.clear(fromOutside);
+                    app.clear(fromOutside);
                 } else if (msg.type === 'draw') {
-                    window.app.draw(msg.x, msg.y, 8, msg.colour, fromOutside);
+                    app.draw(msg.x, msg.y, 8, msg.colour, fromOutside);
                 } else {
                     console.log("Unknown type: " + msg.type);
                 }
             }
         });
-
     }
     $("#whiteboard").on("vmousemove", function(event) {
-        if (window.app.drawOn) {
+        if (app.drawOn) {
             var x = event.pageX;
             var y = event.pageY - $("canvas").offset().top;
-            window.app.draw(x, y, window.app.size, window.app.colour);
+            app.draw(x, y, app.size, app.colour);
         }
         event.preventDefault();
     });
     $("#whiteboard").on("vmousedown", function(event) {
         var x = event.pageX;
         var y = event.pageY - $("canvas").offset().top;
-        window.app.draw(x, y, window.app.size, window.app.colour);
-        window.app.drawOn = true;
+        app.draw(x, y, app.size, app.colour);
+        app.drawOn = true;
     });
     $("#whiteboard").on("vmouseup", function(event) {
-        window.app.drawOn = false;
+        app.drawOn = false;
     });
     $("#whiteboard").bind("tap", function(event) {
         event.preventDefault();
